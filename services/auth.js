2d8d16c9393c3097b4860bc879afa1ecddf7be18
Monika <moni_bonboni90@hotmail.com>
2019-12-08 20:02:19 +0100

    3

---
+++ services/auth.js
@@ -0,0 +1,46 @@
+const express=require ('express')
+const bodyParser=require('body-parser')
+var jwt=require('express-jwt')
+const config=require('../config/index.js')
+const db=require ('../db/connection')
+const auth=require('../handlers/auth')
+const path=require('path')
+
+db.init(config.getConfig('db'))
+
+var api=express()
+var pub=path.join(__dirname,'..','public')
+
+api.use('public',express.static(pub))
+api.use(bodyParser.json())
+
+api.use(
+    jwt(
+        {secret:config.getConfig('jwt').key}
+    ).unless(
+        {path:['/api/v1/register','/api/v1/login','/public']}
+    )
+)
+api.post('/api/v1/register',auth.register)
+api.post('/api/v1/login',auth.login)
+api.get('/api/v1/renew',auth.renew)
+api.post('/api/v1/reset-link',auth.resetLink)
+api.post('/api/v1/reset-password',auth.resetPassword)
+api.post('/api/v1/change-password',auth.changePasswor)
+
+api.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send({ message: 'Invalid token' });
+    } else {
+        next(err);
+    }
+});
+
+api.listen(8081, err => {
+    if (err) {
+        console.log('Could not start server');
+        console.log(err);
+        return;
+    }
+    console.log('Server successfully started on port 8081');
+});
